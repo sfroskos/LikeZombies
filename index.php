3955ccd6d51981eda38bd90cc0edc4a22e295c19
SFR <>
2014-08-14 16:31:17 -0700

    Added FacebookRedirectLoginHelper and afterlogin.php

--- index.php
+++ index.php
@@ -19,6 +19,7 @@ include_once('FacebookAuthorizationException.php' );
 include_once('GraphObject.php' );
 include_once('GraphSessionInfo.php' );
 include_once('AccessToken.php');
+include once('FacebookRedirectLoginHelper');
 use Facebook\FacebookSession;
 use Facebook\FacebookRequest;
 use Facebook\GraphUser;
@@ -29,20 +30,45 @@ use Facebook\GraphObject;
 //Set application variables for accessing Facebook
 \Facebook\FacebookSession::setDefaultApplication('1432542257021113','cc001cfeefbf0fa75256e0c93aaedd29');
 //Get an application session
-$session = \Facebook\FacebookSession::newAppSession();
+//$session = \Facebook\FacebookSession::newAppSession();
 
-//Login user
-$helper = new FacebookCanvasLoginHelper();
+//Login user with FacebookRedirectLoginHelper
+$scope = array('manage_pages, read_stream');
+$helper = new FacebookRedirectLoginHelper('https://likezombiesgame.com/afterlogin.php');
+$loginurl = $helper->getLoginUrl($scope);
+echo '<a href="' . $loginurl . '">Login</a>';
+    
+//Get user name from FB Profile
 try {
-  $session = $helper->getSession();
+    $session = $helper->getSessionFromRedirect();
 } catch(FacebookRequestException $ex) {
-  // When Facebook returns an error
-} catch(\Exception $ex) {
-  // When validation fails or other local issues
+    // When Facebook returns an error
 }
 if ($session) {
-  // Logged in
+    try {
+        $user_profile = (new FacebookRequest(
+            $session, 'GET', '/me'
+        ))->execute()->getGraphObject(GraphUser::className());
+ 
+        echo "Name: " . $user_profile->getName();
+    } catch(FacebookRequestException $e) {
+        echo "Exception occured, code: " . $e->getCode();
+        echo " with message: " . $e->getMessage();
+    }   
 }
+
+//Login user with FacebookCanvasLoginHelper
+//$helper = new FacebookCanvasLoginHelper();
+//try {
+//  $session = $helper->getSession();
+//} catch(FacebookRequestException $ex) {
+  // When Facebook returns an error
+//} catch(\Exception $ex) {
+  // When validation fails or other local issues
+//}
+//if ($session) {
+  // Logged in
+//}
 
 // Validate the session:
 try {
