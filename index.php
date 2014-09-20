b363a0d813c30a78993c4057224b1397e4b543eb
amandamountain <amandamountain@192.168.1.110>
2014-09-19 17:26:12 -0700

    changed to use FacebookCanvasLoginHelper

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ require_once( 'Facebook/FacebookRequestException.php' );
 require_once( 'Facebook/FacebookAuthorizationException.php' );
 require_once( 'Facebook/GraphObject.php' );
 require_once( 'Facebook/GraphUser.php' );
+require_once( 'Facebook/FacebookSignedRequestFromInputHelper.php' );
+require_once( 'Facebook/FacebookCanvasLoginHelper.php' );
 
 use Facebook\FacebookSession;
 use Facebook\FacebookRedirectLoginHelper;
@@ -18,29 +20,21 @@ use Facebook\FacebookRequestException;
 use Facebook\FacebookAuthorizationException;
 use Facebook\GraphObject;
 use Facebook\GraphUser;
+use Facebook\FacebookSignedRequestFromInputHelper;
+use Facebook\FacebookCanvasLoginHelper;
 
 if (!isset($_SESSION['facebookUserId']) || !isset($_SESSION['facebookSession']) || !isset($_SESSION['facebookUserProfile'])) {
     // init app with app id (APPID) and secret (SECRET)
 //    $FacebookSession = new FacebookSession($_SESSION['facebookSession']);
     FacebookSession::setDefaultApplication('1432542257021113','cc001cfeefbf0fa75256e0c93aaedd29');
-    // login helper with redirect_uri
-    $helper = new FacebookRedirectLoginHelper( 'https://apps.facebook.com/likezombies.dev' );
-    try {
-         $FBSession = $helper->getSessionFromRedirect();
+    // login helper
+    //$helper = new FacebookRedirectLoginHelper( 'https://apps.facebook.com/likezombies.dev' );
+    $helper = new FacebookCanvasLoginHelper();
+   try {
+         $FBSession = $helper->getSession();
     } catch( FacebookRequestException $ex ) {
         // When Facebook returns an error
     } catch( Exception $ex ) {
          // When validation fails or other local issues
     }
-    if (!isset($FBSession)) {
-        // STEP ONE - REDIRECT THE USER TO FACEBOOK FOR AUTO LOGIN / APP APPROVAL
-        header('Location: ' . $helper->getLoginUrl());
-        exit();
-    } else {
-        $user_profile = (new FacebookRequest($FBSession, 'GET', '/me'))->execute()->getGraphObject(GraphUser::className());
-        $_SESSION['facebookUserId'] = $user_profile->getID();
-        $_SESSION['facebookSession'] = $FBSession; // I DON'T THINK THIS ACTAULLY WORKS RIGHT
-        $_SESSION['facebookUserProfile'] = $user_profile;
-
-    }
-}
+}    
