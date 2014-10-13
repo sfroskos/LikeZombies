5932327d52776a426140b414ac121a032a0b9adf
SFR <>
2014-10-13 13:31:12 -0700

    fixed redirect url in index.php (added .com)

--- index.php
+++ index.php
@@ -57,7 +57,7 @@ $debug = new PHPDebug();
 //    }
 //    else {
     // Login failed. Redirect user to log in to LikeZombies
-        $helper = new FacebookRedirectLoginHelper('https://likezombiesgame/LoginSuccess.php');
+        $helper = new FacebookRedirectLoginHelper('https://likezombiesgame.com/LoginSuccess.php');
         $FBloginUrl = $helper->getLoginUrl();
         $debug->debug("FBloginUrl = ", $FBloginUrl);
     // Use the login url to redirect to Facebook for authentication
