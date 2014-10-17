77e01afe1e6f2306e2afc4f4ecadc87c18cf6c18
SFR <>
2014-10-17 12:52:59 -0700

    Added debug instantiation statement back to LoginSuccess.php

--- LoginSuccess.php
+++ LoginSuccess.php
@@ -46,8 +46,8 @@ use Facebook\PHPDebug;
 
 FacebookSession::setDefaultApplication('1432542257021113','cc001cfeefbf0fa75256e0c93aaedd29');
 $helper = new FacebookCanvasLoginHelper();
-//$debug = new PHPDebug();
-//$debug->debug("Trying helper->getSession", null, INFO);
+$debug = new PHPDebug();
+$debug->debug("Trying helper->getSession", null, INFO);
 try {
     $FBSession = $helper->getSession();
     $debug->debug("Trying helper->getSession", null, INFO);
