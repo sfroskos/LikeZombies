c4e7869edbbbba83df43fd1b25bee1d60523ec81
SFR <>
2014-10-17 13:47:42 -0700

    moved html from LoginSuccess.php to LoginSuccess.html

--- LoginSuccess.php
+++ LoginSuccess.php
@@ -1,17 +1,3 @@
-<html> 
-<link rel="stylesheet" type="text/css" href="style.css">
-    <head> 
-        <title>Login</title> 
-    </head> 
-    <body id="body-color"> 
-        <div id="LikeZombiesGame"> 
-            <fieldset style="width:30%">
-                <h1>Success! You are now logged into the LikeZombies Game!</h1>
-                <a href="Register.php">Register</a> <a href="Logout.php">Logout</a>
-            </fieldset> 
-        </div> 
-    </body> 
-</html>
 <?php
 require_once( 'Facebook/FacebookSession.php' );
 require_once( 'Facebook/FacebookRedirectLoginHelper.php' );
